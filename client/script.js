f9946a894b075b099b1e2c42b30c7418cb5096aa
Felix Man <felixwhman@gmail.com>
2024-02-19 00:18:00 -0500

    Initial Commit

--- client/script.js
+++ client/script.js
@@ -4,9 +4,6 @@ const appKey = process.env.APPKEY
 
 const baseUrl = `https://api.edamam.com/api/food-database/v2/parser?app_id=${appId}&app_key=${appKey}`
 
-// To get nutritional value use
-// https://api.edamam.com/api/food-database/v2/parser?app_id=a534495c&app_key=09ef8301472e328aad72dce617f165c8&
-
 async function getNutritionalValue() {
 	const url = `${baseUrl}&`
 }
